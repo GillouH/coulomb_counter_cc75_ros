c92976fde2ac7bb2d0766e69313064bb393e94e2
Gillou100 <gilles.hubert@isen.yncrea.fr>
2019-11-28 14:04:29 +0100

    Simplify scripts.

--- scripts/create_udev_rules.sh
+++ scripts/create_udev_rules.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 echo "remap the device serial port(ttyUSBX) to coulomb_counter_cc75"
-echo "coulomb_counter_cc75 usb connection as /dev/coulomb_counter_cc75, check it using the command : ls -l /dev|grep ttyUSB"
-echo "start copy coulomb_counter_cc75.rules to  /lib/udev/rules.d/"
-echo "`rospack find coulomb_counter_cc75_ros`/scripts/coulomb_counter_cc75.rules"
+echo "sudo cp `rospack find coulomb_counter_cc75_ros`/scripts/coulomb_counter_cc75.rules  /lib/udev/rules.d"
 sudo cp `rospack find coulomb_counter_cc75_ros`/scripts/coulomb_counter_cc75.rules  /lib/udev/rules.d
 echo " "
 echo "Restarting udev"
-echo ""
+echo "sudo service udev reload"
 sudo service udev reload
+echo "sudo service udev restart"
 sudo service udev restart
-echo "finish "
+echo "finish"
--- scripts/delete_udev_rules.sh
+++ scripts/delete_udev_rules.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 echo "delete remap the device serial port(ttyUSBX) to coulomb_counter_cc75"
-echo "sudo rm   /lib/udev/rules.d/coulomb_counter_cc75.rules"
-sudo rm   /lib/udev/rules.d/coulomb_counter_cc75.rules
+echo "sudo rm /lib/udev/rules.d/coulomb_counter_cc75.rules"
+sudo rm /lib/udev/rules.d/coulomb_counter_cc75.rules
 echo " "
 echo "Restarting udev"
-echo ""
+echo "sudo service udev reload"
 sudo service udev reload
+echo "sudo service udev restart"
 sudo service udev restart
-echo "finish  delete"
+echo "finish delete"
